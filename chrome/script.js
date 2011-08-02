ce08b893219c7ae2e939850b0c360f4ae7b34cf3
nkimkha@gmail.com <nkimkha@gmail.com@96dd450a-8c7c-a73d-28bf-e8439c8a685c>
2011-08-02 05:11:42 +0000

    update for chrome

--- chrome/script.js
+++ chrome/script.js
@@ -4,7 +4,7 @@ function fetchMeaning(text) {
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
       if (xhr.status == 200) {
-        onReceive(xhr.responseText);
+        onReceive(escape(xhr.responseText));
       } else {
         onReceive(null);
       }
@@ -18,31 +18,78 @@ function fetchMeaning(text) {
 
 function onReceive(data) {
 	if (data) {
-		var root = document.createElement('div');
-		root.style.cssText = [
-		  'background-color: #CCEFFF;',
-		  'background-image: -webkit-repeating-linear-gradient(' +
-		      '45deg, transparent, transparent 35px,' +
-		      'rgba(150,150,150,.1) 35px, rgba(150,150,150,.1) 70px);',
-		  'color: #000;',
-		  'width: 300px;',
-		  'height: 160px;',
-		  'overflow-y: auto;',
-		  'padding: 10px;',
-		  'font: 14px Arial;',
-		  'position: fixed;',
-		  'top: 20px;',
-		  'left: 20px;',
-		  'z-index: 10000;'
-		].join(' ');
-		root.innerHTML = data;
-		document.body.parentElement.insertBefore(root, document.body);
+		var el = document.getElementById("kdict-content");
+		if (el) {
+			el.innerHTML = unescape(data);
+		}
 	}
 };
 
 function callTranslate(request, sender, sendResponse) {
-	var text = request.seletedText;
+	var text = request.selectedText;
 	fetchMeaning(text);
+	
+	var root = document.createElement('div');
+	var head = document.createElement('div');
+	var content = document.createElement('div');
+	var close = document.createElement('div');
+	
+	var closeImg = chrome.extension.getURL("images/close.gif");
+	
+	root.id = "kdict-root";
+	root.style.cssText = [
+	  'background-color: #CCEFFF;',
+	  'background-image: -webkit-repeating-linear-gradient(' +
+	      '45deg, transparent, transparent 35px,' +
+	      'rgba(150,150,150,.1) 35px, rgba(150,150,150,.1) 70px);',
+	  'color: #000;',
+	  'width: 300px;',
+	  'font: 14px Arial;',
+	  'position: fixed;',
+	  'top: 20px;',
+	  'left: 20px;',
+	  'z-index: 10000;'
+	].join(' ');
+	
+	head.style.cssText = [
+		'width: 275px;',
+		'font-size: 15px;',
+		'font-weight: bold;',
+		'padding: 0 5px;',
+		'display: block;',
+		'float: left;'
+	].join(' ');
+	head.innerText = "Nghĩa của từ: '" + text + "'";
+	
+	close.innerHTML = "<img src='" + closeImg + "' border=0 style='margin:0;padding:0;width:10px;height:10px;' />";
+	close.style.cssText = [
+		'width: 12px;',
+		'height: 12px;',
+		'padding: 3px 0 0 0;',
+		'display: block;',
+		'float: right;',
+		'cursor: pointer;'
+	].join(' ');
+	close.onclick = function() {
+		var el = document.getElementById("kdict-root");
+		el.parentNode.removeChild(el);
+	}
+	
+	content.id = "kdict-content";
+	content.style.cssText = [
+		'height: 160px;',
+		'width: 270px;',
+		'overflow-y: auto;',
+		'clear: both;',
+		'padding: 10px;',
+		'margin: 5px;'
+	].join(' ');
+	content.innerHTML = "Đang đọc dữ liệu...";
+	
+	root.appendChild(head);
+	root.appendChild(close);
+	root.appendChild(content);
+	document.body.parentElement.insertBefore(root, document.body);
 }
 
 chrome.extension.onRequest.addListener(callTranslate);
